2d2749b747d746d99c5b51faff793aac24552edb
zil-web <126854462+zil-web@users.noreply.github.com>
2024-04-08 13:08:30 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+VideoColorSpace.addEventListener("click", function video(){
+    alert("ini video Red Velvet");
+})
+
+Audio.addEventListener("click", function audio(){
+    alert("ini Audio Rel Velvet");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Red Velvet");
+})
